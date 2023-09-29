3e8e0459dbd609820ad650a934500d2d247bcd4b
akiomatic018 <dokuringo.dev@gmail.com>
2023-09-28 23:29:36 -0700

    feat: Used recursive calls to solve binarySearch

--- 102/binarySearch.js
+++ 102/binarySearch.js
@@ -27,6 +27,71 @@ function binarySearch(arr, target) {
 }
 
 
+// Trying to make it work in a diffrent method, using a recursive call
+function binarySearchWithRecursion(arr, target) {
+
+    let left = 0, right = arr.length - 1, middle = Math.floor((left + right) / 2);
+
+    function recursive() {
+        if (left > right) return;
+        if (arr[middle] === target) return;
+
+        if (left <= right) {
+            if (target <= arr[middle]) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+            middle = Math.floor((left + right) / 2);
+            recursive();
+        }
+
+       return;
+    }
+
+    recursive();
+
+    if (left <= right) {
+        return middle;
+    }
+
+    return -1;
+}
+
+
+// Here is the improved version of the function above by ChatGPT
+// I definitely need some practice to get used to the recursive calls:)
+function binarySearchWithRecursionByChatGPT(arr, target) {
+    function recursive(left, right) {
+        if (left > right) return -1;
+        
+        let middle = Math.floor((left + right) / 2);
+        
+        if (arr[middle] === target) return middle;
+
+        if (target < arr[middle]) {
+            return recursive(left, middle - 1);
+        } else {
+            return recursive(middle + 1, right);
+        }
+    }
+
+    return recursive(0, arr.length - 1);
+}
+
+
 console.log(binarySearch([1,2,3,4,5,6,7,8], 3)); //2
 console.log(binarySearch([1,2,3,4,5,6,7,8], 11)); //-1
 console.log(binarySearch([1,2,3,4,5,6,7,8], 8)); //7
+
+console.log("------------------------------");
+
+console.log("Test1 ", binarySearchWithRecursion([1,2,3,4,5,6,7,8], 3)); //2
+console.log("Test2 ", binarySearchWithRecursion([1,2,3,4,5,6,7,8], 11)); //-1
+console.log("Test3 ", binarySearchWithRecursion([1,2,3,4,5,6,7,8], 8)); //7
+
+console.log("------------------------------");
+
+console.log("TestGPT1 ", binarySearchWithRecursionByChatGPT([1,2,3,4,5,6,7,8], 3)); //2
+console.log("TestGPT2 ", binarySearchWithRecursionByChatGPT([1,2,3,4,5,6,7,8], 11)); //-1
+console.log("TestGPT3 ", binarySearchWithRecursionByChatGPT([1,2,3,4,5,6,7,8], 8)); //7
